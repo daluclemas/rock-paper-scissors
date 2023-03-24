e1f7eb70034656a4c540d1c522f83c17fbeb08f6
daluclemas <chidaluanukam@gmail.com>
2023-03-24 08:57:28 +0100

    inital comiit and repo setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import * as pages from "./pages";
+import { paths } from "./paths";
+
+function App() {
+  return (
+    <div className="App border-2 border-red-500">
+      <RoutesContainer />
+    </div>
+  );
+}
+
+function RoutesContainer() {
+  return (
+    <Routes>
+      <Route element={<pages.Home />} path={paths.home} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1 className="capitalize">vite day</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+export default index;
